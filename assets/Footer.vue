0d6296f520e14dfff600b2e3dc6874221388336c
凉城 <lcdl6@qq.com>
2025-08-06 13:40:53 +0800

    Update Footer.vue

--- assets/Footer.vue
+++ assets/Footer.vue
@@ -15,23 +15,7 @@
       </a>
     </div>
 
-    <div class="stats">
-      访问次数：<span id="vercount_value_site_pv">😯</span>次 | 访客人数：<span id="vercount_value_site_uv">😯</span>人
-    </div>
-
-    <div class="project-links">
-      <strong>项目:</strong><a href="https://github.com/willow-god/FlareDrive-R2" target="_blank"
-        rel="noopener noreferrer">FlareDrive-R2</a>
-      <strong>作者:</strong><a href="https://github.com/willow-god" target="_blank"
-        rel="noopener noreferrer">LiuShen</a>
-      <strong>初始项目:</strong><a href="https://github.com/ljxi/Cloudflare-R2-oss" target="_blank"
-        rel="noopener noreferrer">Cloudflare-R2-OSS</a>
-    </div>
-
-    <div class="powered">Powered by <a href="https://workers.cloudflare.com/" target="_blank"
-        rel="noopener noreferrer">Cloudflare Workers</a></div>
-
-    <div style="margin:10px 0;">© 2025 FlareDrive. All rights reserved.</div>
+    <div style="margin:10px 0;">© 2025 凉城. All rights reserved.</div>
   </footer>
 </template>
 
@@ -42,7 +26,7 @@ export default {
     return {
       homeUrl: "https://pan.20071008.xyz/",
       blogUrl: "https://20071008.xyz/",
-      githubUrl: "https://github.com/lcdl6",
+      githubUrl: "https://github.com/sumerf",
       emailUrl: "mailto:lcdl6@qq.com"
     };
   }
